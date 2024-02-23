80e1ed91c40e71c28896c69af8dee5800e00edc7
Leonardo Oliveira <leonardo.viegaso@gmail.com>
2024-02-23 16:23:23 -0300

    add botao de reinicio e comando game over para encerrar

--- app/validacao.js
+++ app/validacao.js
@@ -2,8 +2,16 @@ function verificaSeOChutePossuiUmValorValido(chute){
     const numero = +chute
 
     if (chuteForInvalido(numero)){
-        elementoChute.innerHTML += `<div>Numero invalido</div>`
-        return
+        if (chute.toUpperCase() === "GAME OVER"){
+            document.body.innerHTML = `
+                <h2> Game Over!!!</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+            `
+            document.body.style.backgroundColor = "black";
+        } else {
+            elementoChute.innerHTML += `<div>Numero invalido</div>`
+        }
     }
 
     if (ChuteForaDoIntervalo(numero)){
@@ -17,6 +25,7 @@ function verificaSeOChutePossuiUmValorValido(chute){
         document.body.innerHTML = `
         <h2>Você acertou!</h2>
         <h3>O número secreto era ${numeroSecreto}</h3>
+        <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
         `
     } else if (numero > numeroSecreto){
         elementoChute.innerHTML += `
@@ -36,3 +45,9 @@ function chuteForInvalido (numero) {
 function ChuteForaDoIntervalo(numero) {
     return numero > maiorValor || numero < menorValor
 }
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
